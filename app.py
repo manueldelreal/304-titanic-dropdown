bc0a75e24d89c7a1b688238f06d560fc3857abf5
Manuel Del Real <manueldelreal@gmail.com>
2022-08-09 17:54:13 +0000

    Added male passenger variable to analysis

--- app.py
+++ app.py
@@ -19,9 +19,10 @@ githublink = 'https://github.com/manueldelreal/304-titanic-dropdown'
 
 ###### Import a dataframe #######
 df = pd.read_csv("https://raw.githubusercontent.com/austinlasseter/plotly_dash_tutorial/master/00%20resources/titanic.csv")
+df['Male']=df['Sex'].map({'male':1, 'female':0})
 df['Female']=df['Sex'].map({'male':0, 'female':1})
 df['Cabin Class'] = df['Pclass'].map({1:'first', 2: 'second', 3:'third'})
-variables_list=['Survived', 'Female', 'Fare', 'Age']
+variables_list=['Survived', 'Male', 'Female', 'Age']
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
